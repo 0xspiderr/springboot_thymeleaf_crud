5ae203d460af5931553e88539571870776b8a246
Harry <harypatrascu.1@gmail.com>
2025-12-14 15:07:51 +0200

    Added car filters for 3 attributes

--- springboot_thymeleaf_crud/src/main/java/com/project/springboot_thymeleaf_crud/Masina/MasinaController.java
+++ springboot_thymeleaf_crud/src/main/java/com/project/springboot_thymeleaf_crud/Masina/MasinaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,13 +16,31 @@ public class MasinaController
     MasinaRepository repository;
 
     @GetMapping("/masini")
-    public String get_lista_masini(Model model)
+    public String get_lista_masini(Model model,
+                                   @RequestParam(required = false) String marca,
+                                   @RequestParam(required = false) String culoare,
+                                   @RequestParam(required = false) String combustibil)
     {
-        String str = "Lista masinilor preluate prin repository";
+        String filtru_marca = (marca != null && !marca.isEmpty()) ? marca.toLowerCase() : null;
+        String filtru_culoare = (culoare != null && !culoare.isEmpty()) ? culoare.toLowerCase() : null;
+        String filtru_combustibil = (combustibil != null && !combustibil.isEmpty()) ? combustibil.toLowerCase() : null;
+
+        List<Masina> masini = repository.findAll();
+
+        List<Masina> lista_filtrata = masini.stream()
+                .filter(m -> m.getMarca().equals(filtru_marca) || filtru_marca == null)
+                .filter(m -> m.getCuloarea().equals(filtru_culoare) || filtru_culoare == null)
+                .filter(m -> m.getTipulDeCombustibil().equals(filtru_combustibil) || filtru_combustibil == null)
+                .toList();
+
+
+        String str = "Toate masinile";
         model.addAttribute("str", str);
-        model.addAttribute("lista", repository.findAll());
+        model.addAttribute("lista", lista_filtrata);
+        model.addAttribute("marcaSelectata", marca);
+        model.addAttribute("culoareaSelectata", culoare);
+        model.addAttribute("combustibilSelectat", combustibil);
+
         return "masini";
     }
-
-
 }
--- springboot_thymeleaf_crud/src/main/resources/data.sql
+++ springboot_thymeleaf_crud/src/main/resources/data.sql
@@ -11,3 +11,17 @@ cuplul,
 volumul_portbagajului,
 pretul)
 values ("TM12AAA",1,"ford","mondeo","albastru",2014,2000,"electric",140,180,600,9000);
+
+insert into masini(nr_inmatriculare,
+id_utilizator,
+marca,
+modelul,
+culoarea,
+anul_fabricatiei,
+capacitatea_cilindrica,
+tipul_de_combustibil,
+puterea,
+cuplul,
+volumul_portbagajului,
+pretul)
+values ("TM14CSS",1,"seat","leon","galben",2015,1800,"benzina",150,120,350,10500);
