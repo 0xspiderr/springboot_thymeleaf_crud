b69b2dd708502b2a439702c0eb850005cced821e
Harry <harypatrascu.1@gmail.com>
2025-12-14 14:30:35 +0200

    Added html template "masini" that shows a table of all cars in the DB

--- springboot_thymeleaf_crud/src/main/java/com/project/springboot_thymeleaf_crud/Masina/Masina.java
+++ springboot_thymeleaf_crud/src/main/java/com/project/springboot_thymeleaf_crud/Masina/Masina.java
@@ -1,166 +1,36 @@
 package com.project.springboot_thymeleaf_crud.Masina;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
 @Entity
 @Table(name="masini")
 public class Masina
 {
     @Id
-    private String nr_inmatriculare;
-    private int id_utilizator;
+    @Column(name = "nr_inmatriculare")
+    private String nrInmatriculare;
+    @Column(name = "id_utilizator")
+    private int idUtilizator;
     private String marca;
-    private String modelul;
+    @Column(name = "modelul")
+    private String model;
     private String culoarea;
-    private int anul_fabricatiei;
-    private int capacitatea_cilindrica;
-    private String tipul_de_combustibil;
+    @Column(name = "anul_fabricatiei")
+    private int anulFabricatiei;
+    @Column(name = "capacitatea_cilindrica")
+    private int capacitateaCilindrica;
+    @Column(name = "tipul_de_combustibil")
+    private String tipulDeCombustibil;
     private int puterea;
     private int cuplul;
-    private int volumul_portbagajului;
-    private int pretul;
-
-    public Masina(String nr_inmatriculare,
-                  int id_utilizator,
-                  String marca,
-                  String modelul,
-                  String culoarea,
-                  int anul_fabricatiei,
-                  int capacitatea_cilindrica,
-                  String tipul_de_combustibil,
-                  int puterea, int cuplul,
-                  int volumul_portbagajului,
-                  int pretul) {
-        this.nr_inmatriculare = nr_inmatriculare;
-        this.id_utilizator = id_utilizator;
-        this.marca = marca;
-        this.modelul = modelul;
-        this.culoarea = culoarea;
-        this.anul_fabricatiei = anul_fabricatiei;
-        this.capacitatea_cilindrica = capacitatea_cilindrica;
-        this.tipul_de_combustibil = tipul_de_combustibil;
-        this.puterea = puterea;
-        this.cuplul = cuplul;
-        this.volumul_portbagajului = volumul_portbagajului;
-        this.pretul = pretul;
-    }
-
-    public Masina() {
-    }
-
-    public String getNr_inmatriculare() {
-        return nr_inmatriculare;
-    }
-
-    public void setNr_inmatriculare(String nr_inmatriculare) {
-        this.nr_inmatriculare = nr_inmatriculare;
-    }
-
-    public int getId_utilizator() {
-        return id_utilizator;
-    }
-
-    public void setId_utilizator(int id_utilizator) {
-        this.id_utilizator = id_utilizator;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getModelul() {
-        return modelul;
-    }
-
-    public void setModelul(String modelul) {
-        this.modelul = modelul;
-    }
-
-    public String getCuloarea() {
-        return culoarea;
-    }
-
-    public void setCuloarea(String culoarea) {
-        this.culoarea = culoarea;
-    }
-
-    public int getAnul_fabricatiei() {
-        return anul_fabricatiei;
-    }
-
-    public void setAnul_fabricatiei(int anul_fabricatiei) {
-        this.anul_fabricatiei = anul_fabricatiei;
-    }
-
-    public int getCapacitatea_cilindrica() {
-        return capacitatea_cilindrica;
-    }
-
-    public void setCapacitatea_cilindrica(int capacitate_cilindrica) {
-        this.capacitatea_cilindrica = capacitate_cilindrica;
-    }
-
-    public String getTipul_de_combustibil() {
-        return tipul_de_combustibil;
-    }
-
-    public void setTipul_de_combustibil(String tipul_de_combustibil) {
-        this.tipul_de_combustibil = tipul_de_combustibil;
-    }
-
-    public int getPuterea() {
-        return puterea;
-    }
-
-    public void setPuterea(int puterea) {
-        this.puterea = puterea;
-    }
-
-    public int getCuplul() {
-        return cuplul;
-    }
-
-    public void setCuplul(int cuplul) {
-        this.cuplul = cuplul;
-    }
-
-    public int getVolumul_portbagajului() {
-        return volumul_portbagajului;
-    }
-
-    public void setVolumul_portbagajului(int volumul_portbagajului) {
-        this.volumul_portbagajului = volumul_portbagajului;
-    }
-
-    public int getPretul() {
-        return pretul;
-    }
-
-    public void setPretul(int pretul) {
-        this.pretul = pretul;
-    }
-
-    @Override
-    public String toString() {
-        return "Masina{" +
-                "nr_inmatriculare=" + nr_inmatriculare +
-                ", id_utilizator=" + id_utilizator +
-                ", marca='" + marca + '\'' +
-                ", modelul='" + modelul + '\'' +
-                ", culoarea='" + culoarea + '\'' +
-                ", anul_fabricatiei=" + anul_fabricatiei +
-                ", capacitate_cilindrica=" + capacitatea_cilindrica +
-                ", tipul_de_combustibil=" + tipul_de_combustibil +
-                ", puterea=" + puterea +
-                ", cuplul=" + cuplul +
-                ", volumul_portbagajului=" + volumul_portbagajului +
-                ", pretul=" + pretul +
-                '}';
-    }
+    @Column(name = "volumul_portbagajului")
+    private int volumulPortbagajului;
+    @Column(name = "pretul")
+    private int pret;
 }
--- springboot_thymeleaf_crud/src/main/java/com/project/springboot_thymeleaf_crud/Masina/MasinaController.java
+++ springboot_thymeleaf_crud/src/main/java/com/project/springboot_thymeleaf_crud/Masina/MasinaController.java
@@ -1,22 +1,27 @@
 package com.project.springboot_thymeleaf_crud.Masina;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class MasinaController
 {
     @Autowired
     MasinaRepository repository;
 
     @GetMapping("/masini")
-    public List<Masina> find_all()
+    public String get_lista_masini(Model model)
     {
-        return repository.findAll();
+        String str = "Lista masinilor preluate prin repository";
+        model.addAttribute("str", str);
+        model.addAttribute("lista", repository.findAll());
+        return "masini";
     }
 
-    
+
 }
